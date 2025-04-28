213f323d9ac42a94d39e552c8105746561426179
Ciposki <francesco.gissara@gmail.com>
2025-04-28 17:40:17 +0200

    Scroll Bar e fit Text added

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  
+  const titlesResize = document.querySelectorAll('.sez h1')
+  titlesResize.forEach(title => (
+      fitText(title)
+  ))
+
+  let nav = document.querySelector(".nav")
   let titles = document.querySelector(".title").querySelectorAll("h1");
   let subtitle = document.querySelector(".subtitle");
-  let nav = document.querySelector(".nav")
+  console.log
   gsap.registerPlugin(ScrollTrigger)
   var startTl = gsap.timeline({defaults: {ease: "expo.out"}})
   
@@ -12,3 +17,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
   startTl.fromTo(nav,{y:"-100%"},{duration:1,y:0},">-0.2")
   startTl.fromTo(subtitle, {opacity:0}, {duration:1.5,opacity:1},">-0.6")
  });
+
+window.onscroll = function() {progress()};
+
+function progress() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").value = scrolled;
+}
